6745a8d7ff4541a25463eda2b1fcd54b18e7012e
Oliver Gonzalez <olgo376@vidalibarraquer.net>
2026-09-23 13:05:06 +0200

    Ativitats basiques finalizada

--- src/main/java/Intercanvi.java
+++ src/main/java/Intercanvi.java
@@ -5,5 +5,14 @@ public class Intercanvi {
         //       variable auxiliar i mostra'ls després de l'intercanvi:
         //   a = ...
         //   b = ...
+        int a = 10;
+        int b = 20;
+
+        int c = a;
+        a = b; 
+        b = c;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
 }
--- src/main/java/MonedesMinimes.java
+++ src/main/java/MonedesMinimes.java
@@ -17,6 +17,42 @@ public class MonedesMinimes {
         //   1 moneda de 1 cèntims
 
         Scanner teclat = new Scanner(System.in);
+        System.out.println("Entra una quantitat en cèntims: ");
+        int centims = teclat.nextInt();
+
+        int monedes200 = centims / 200;
+        centims = centims % 200;
+        // similar al de las horas, para aprovechar el resto
+
+        int monedes100 = centims / 100;
+        centims = centims % 100;
+
+        int monedes50 = centims / 50;
+        centims = centims % 50;
+
+        int monedes20 = centims / 20;
+        centims = centims % 20;
+
+        int monedes10 = centims / 10;
+        centims = centims % 10;
+
+        int monedes5 = centims / 5;
+        centims = centims % 5;
+
+        int monedes2 = centims / 2;
+        centims = centims % 2;
+
+        int monedes1 = centims;
+
+        System.out.println(monedes200 + "monedes de 2 euros");
+        System.out.println(monedes100 + "monedes d'1 euro");
+        System.out.println(monedes50 + "monedes de 50 cèntims");
+        System.out.println(monedes20 + "monedes de 20 cèntims");
+        System.out.println(monedes10 + "monedes de 10 cèntims");
+        System.out.println(monedes5 + "monedes de 5 cèntims");
+        System.out.println(monedes2 + "monedes de 2 cèntims");
+        System.out.println(monedes1 + "monedes de 1 cèntim");
+
         
     }
 }
